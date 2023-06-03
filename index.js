1027c4d182dc9746ff59c11296a63a88ad4844ab
bianoxs <125635314+bianoxs@users.noreply.github.com>
2023-06-03 23:00:36 +0000

    Updated Dutch translation by Wouter Bolsterlee.

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ import { ChatGPTAPI } from "chatgpt";
 
 const require = createRequire(import.meta.url);
 const config = require("./config.json");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+var processing = false;
